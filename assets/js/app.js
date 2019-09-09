8ec4c4baf31d0f8e57027220d828a9a98f86c0ad
JosefinaAureaAmaro <josie.a.amaro@gmail.com>
2019-09-09 11:45:06 -0400

    finalized csv data parsing code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,40 @@
 // @TODO: YOUR CODE HERE!
 
-// reading csv data
-d3.csv("../data/data.csv").then(function(data) {
-    console.log(data);
-});
+// // reading csv data
+let dataKeys = [];
+let chartData = []; 
+
+
+d3.csv("./assets/data/data.csv").then(function(data) {
+    // to create an array of Keys to be observed 
+    dataKeys = Object.keys(data[0]);
+    // console.log(dataKeys);
+    dataKeys.pop(); // to remove obsecure data point from the keys
+    console.log(dataKeys);
+    // console.log(dataKeys[0]); // to test index of array 
+
+    // to parse csv data 
+    data.forEach(function(d){
+        // to fix data types of the numeric cols 
+        d.id = +d.id; 
+        for (i = 3; i <= dataKeys.length ; i++) {
+            d[dataKeys[i]] = +d[dataKeys[i]];
+        }
+        // to remove obsecure data keys from the object 
+        delete d["-0.385218228"];
+        delete d["undefined"];
+    console.log(d);
+    // to assign finalized array to global variable and set the objects into an array
+    chartData.push(d);
+    })
+}); 
+
+// to view finalized data 
+console.log('-------------')
+console.log(chartData); 
+
+
+
+// helpful cites
+// Parsing csv data: http://learnjsdata.com/read_data.html
+// removing properties from objects: https://stackoverflow.com/questions/3455405/how-do-i-remove-a-key-from-a-javascript-object
